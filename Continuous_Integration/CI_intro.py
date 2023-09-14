88316dc8010198430f35708c2e1437ac93bf2484
trinadh Ravuri <trinadh.ravz@gmail.com>
2023-09-14 16:29:35 +0530

    adding remaining files

---
+++ Continuous_Integration/CI_intro.py
@@ -0,0 +1,16 @@
+'''
+Core concepts and benefits of Continuous Integration(CI)
+Create a continuous integration pipeline
+Use a CI Pipeline to manage a python project
+
+
+What is Continuous Integration:
+    * Is a practice of frequently building and testing every changed piece of code
+        and integrating that code.
+    * Frequently building and testing changes
+    * Single-Source repository of code
+    * Different streams of team of developers and individuals work on different branches but works on same master
+    * Automating building and testing
+    * Since python is interpreted and no need of compiler
+    * External Services
+'''
