fb983c4b9668a8dedc0220f315ff36b2b1fbbcf7
trinadh Ravuri <trinadh.ravz@gmail.com>
2023-09-14 17:05:58 +0530

    adding tests for multiply

--- Continuous_Integration/test_calc.py
+++ Continuous_Integration/test_calc.py
@@ -1,5 +1,8 @@
 """
 unit tests for the calculator library
+
+Writing failing tests and adding code to make them pass
+    is called Test Driven Development
 """
 
 import pytest
@@ -12,3 +15,6 @@ class TestCalculator:
 
     def test_subtraction(self):
         assert 2 == calculator.subtract(4,2)
+
+    def test_multiplication(self):
+        assert 100 == calculator.multiply(10,10)
